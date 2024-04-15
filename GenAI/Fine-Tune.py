82090d4457b121dc24d1a70eff154bb9062c54c1
Paula <32401901+paula1999@users.noreply.github.com>
2024-04-15 13:47:05 +0200

    Create Fine-Tune.py

---
+++ GenAI/Fine-Tune.py
@@ -0,0 +1,507 @@
+# Fine-Tune a Generative AI Model for Dialogue Summarization
+# FLAN-T5
+
+# Load dataset
+huggingface_dataset_name = "knkarthick/dialogsum"
+dataset = load_dataset(huggingface_dataset_name)
+
+'''
+DatasetDict({
+    train: Dataset({
+        features: ['id', 'dialogue', 'summary', 'topic'],
+        num_rows: 12460
+    })
+    validation: Dataset({
+        features: ['id', 'dialogue', 'summary', 'topic'],
+        num_rows: 500
+    })
+    test: Dataset({
+        features: ['id', 'dialogue', 'summary', 'topic'],
+        num_rows: 1500
+    })
+})
+'''
+
+# Load FLAN-T5 model
+model_name='google/flan-t5-base'
+original_model = AutoModelForSeq2SeqLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Number of model parameters
+def print_number_of_trainable_model_parameters(model):
+    trainable_model_params = 0
+    all_model_params = 0
+    for _, param in model.named_parameters():
+        all_model_params += param.numel()
+        if param.requires_grad:
+            trainable_model_params += param.numel()
+    return f"trainable model parameters: {trainable_model_params}\nall model parameters: {all_model_params}\npercentage of trainable model parameters: {100 * trainable_model_params / all_model_params:.2f}%"
+
+print(print_number_of_trainable_model_parameters(original_model))
+
+'''
+trainable model parameters: 247577856
+all model parameters: 247577856
+percentage of trainable model parameters: 100.00%
+'''
+
+
+####################################################
+# Test the Model with Zero Shot Inferencing
+####################################################
+index = 200
+dialogue = dataset['test'][index]['dialogue']
+summary = dataset['test'][index]['summary']
+prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary:
+"""
+
+inputs = tokenizer(prompt, return_tensors='pt')
+output = tokenizer.decode(
+    original_model.generate(
+        inputs["input_ids"], 
+        max_new_tokens=200,
+    )[0], 
+    skip_special_tokens=True
+)
+
+dash_line = '-'.join('' for x in range(100))
+print(dash_line)
+print(f'INPUT PROMPT:\n{prompt}')
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+print(dash_line)
+print(f'MODEL GENERATION - ZERO SHOT:\n{output}')
+
+
+####################################################
+# Full Fine-Tuning
+####################################################
+
+# Preprocess the Dialog-Summary Dataset
+# Convert the dialog-summary (prompt-response) pairs into explicit instructions for the LLM
+
+# Training prompt (dialoge):
+'''
+Summarize the following conversation.
+
+    {dialogue}
+    
+Summary: 
+'''
+
+# Training response (summary):
+'''
+{summary}
+'''
+
+# Then preprocess the prompt-response dataset into tokens and pull out their input_ids (1 per token)
+
+def tokenize_function(example):
+    start_prompt = 'Summarize the following conversation.\n\n'
+    end_prompt = '\n\nSummary: '
+    prompt = [start_prompt + dialogue + end_prompt for dialogue in example["dialogue"]]
+    example['input_ids'] = tokenizer(prompt, padding="max_length", truncation=True, return_tensors="pt").input_ids
+    example['labels'] = tokenizer(example["summary"], padding="max_length", truncation=True, return_tensors="pt").input_ids
+    
+    return example
+
+# The dataset actually contains 3 diff splits: train, validation, test.
+# The tokenize_function code is handling all data across all splits in batches.
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+tokenized_datasets = tokenized_datasets.remove_columns(['id', 'topic', 'dialogue', 'summary',])
+
+# Subsample (optional)
+tokenized_datasets = tokenized_datasets.filter(lambda example, index: index % 100 == 0, with_indices=True)
+
+# Shapes
+print(f"Shapes of the datasets:")
+print(f"Training: {tokenized_datasets['train'].shape}")
+print(f"Validation: {tokenized_datasets['validation'].shape}")
+print(f"Test: {tokenized_datasets['test'].shape}")
+print(tokenized_datasets)
+
+'''
+Shapes of the datasets:
+Training: (125, 2)
+Validation: (5, 2)
+Test: (15, 2)
+DatasetDict({
+    train: Dataset({
+        features: ['input_ids', 'labels'],
+        num_rows: 125
+    })
+    validation: Dataset({
+        features: ['input_ids', 'labels'],
+        num_rows: 5
+    })
+    test: Dataset({
+        features: ['input_ids', 'labels'],
+        num_rows: 15
+    })
+})
+'''
+
+
+# Fine-Tune the Model with the Preprocessed Dataset
+output_dir = f'./dialogue-summary-training-{str(int(time.time()))}'
+
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    learning_rate=1e-5,
+    num_train_epochs=1,
+    weight_decay=0.01,
+    logging_steps=1,
+    max_steps=1
+)
+
+trainer = Trainer(
+    model=original_model,
+    args=training_args,
+    train_dataset=tokenized_datasets['train'],
+    eval_dataset=tokenized_datasets['validation']
+)
+
+# Train
+trainer.train()
+
+# Checkpoint
+!aws s3 cp --recursive s3://dlai-generative-ai/models/flan-dialogue-summary-checkpoint/ ./flan-dialogue-summary-checkpoint/
+!ls -alh ./flan-dialogue-summary-checkpoint/pytorch_model.bin
+instruct_model = AutoModelForSeq2SeqLM.from_pretrained("./flan-dialogue-summary-checkpoint", torch_dtype=torch.bfloat16)
+
+# Evaluate the Model Qualitatively (Human Evaluation)
+index = 200
+dialogue = dataset['test'][index]['dialogue']
+human_baseline_summary = dataset['test'][index]['summary']
+
+prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary:
+"""
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+
+instruct_model_outputs = instruct_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
+
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{human_baseline_summary}')
+print(dash_line)
+print(f'ORIGINAL MODEL:\n{original_model_text_output}')
+print(dash_line)
+print(f'INSTRUCT MODEL:\n{instruct_model_text_output}')
+
+
+# Evaluate the Model Quantitatively (with ROUGE Metric)
+rouge = evaluate.load('rouge')
+
+# Generate the outputs for the sample of the test dataset
+dialogues = dataset['test'][0:10]['dialogue']
+human_baseline_summaries = dataset['test'][0:10]['summary']
+
+original_model_summaries = []
+instruct_model_summaries = []
+
+for _, dialogue in enumerate(dialogues):
+    prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary: """
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+    original_model_summaries.append(original_model_text_output)
+
+    instruct_model_outputs = instruct_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
+    instruct_model_summaries.append(instruct_model_text_output)
+    
+zipped_summaries = list(zip(human_baseline_summaries, original_model_summaries, instruct_model_summaries))
+ 
+df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'original_model_summaries', 'instruct_model_summaries'])
+df
+
+# Evaluate the models computing ROUGE metrics
+original_model_results = rouge.compute(
+    predictions=original_model_summaries,
+    references=human_baseline_summaries[0:len(original_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+instruct_model_results = rouge.compute(
+    predictions=instruct_model_summaries,
+    references=human_baseline_summaries[0:len(instruct_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('ORIGINAL MODEL:')
+print(original_model_results)
+print('INSTRUCT MODEL:')
+print(instruct_model_results)
+
+
+# Using a pre-populated list of all model results which you can use to evaluate on a larger section of data
+results = pd.read_csv("data/dialogue-summary-training-results.csv")
+
+human_baseline_summaries = results['human_baseline_summaries'].values
+original_model_summaries = results['original_model_summaries'].values
+instruct_model_summaries = results['instruct_model_summaries'].values
+
+original_model_results = rouge.compute(
+    predictions=original_model_summaries,
+    references=human_baseline_summaries[0:len(original_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+instruct_model_results = rouge.compute(
+    predictions=instruct_model_summaries,
+    references=human_baseline_summaries[0:len(instruct_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('ORIGINAL MODEL:')
+print(original_model_results)
+print('INSTRUCT MODEL:')
+print(instruct_model_results)
+
+print("Absolute percentage improvement of INSTRUCT MODEL over ORIGINAL MODEL")
+
+improvement = (np.array(list(instruct_model_results.values())) - np.array(list(original_model_results.values())))
+for key, value in zip(instruct_model_results.keys(), improvement):
+    print(f'{key}: {value*100:.2f}%')
+
+
+
+
+####################################################
+# Perform Parameter Efficient Fine-Tuning (PEFT)
+####################################################
+
+# Setup the PEFT/LoRA model for Fine-Tuning with a new laayer/parameter adapter
+from peft import LoraConfig, get_peft_model, TaskType
+
+lora_config = LoraConfig(
+    r=32, # Rank
+    lora_alpha=32,
+    target_modules=["q", "v"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type=TaskType.SEQ_2_SEQ_LM # FLAN-T5
+)
+
+# Add LoRA adapter layers/parameters to the original LLM to be trained.
+peft_model = get_peft_model(original_model, lora_config)
+print(print_number_of_trainable_model_parameters(peft_model))
+
+
+# Train PEFT Adapter
+output_dir = f'./peft-dialogue-summary-training-{str(int(time.time()))}'
+
+peft_training_args = TrainingArguments(
+    output_dir=output_dir,
+    auto_find_batch_size=True,
+    learning_rate=1e-3, # Higher learning rate than full fine-tuning.
+    num_train_epochs=1,
+    logging_steps=1,
+    max_steps=1    
+)
+    
+peft_trainer = Trainer(
+    model=peft_model,
+    args=peft_training_args,
+    train_dataset=tokenized_datasets["train"],
+)
+
+peft_trainer.train()
+
+# Save
+peft_model_path="./peft-dialogue-summary-checkpoint-local"
+peft_trainer.model.save_pretrained(peft_model_path)
+tokenizer.save_pretrained(peft_model_path)
+
+# Cecckpoint
+!aws s3 cp --recursive s3://dlai-generative-ai/models/peft-dialogue-summary-checkpoint/ ./peft-dialogue-summary-checkpoint-from-s3/ 
+!ls -al ./peft-dialogue-summary-checkpoint-from-s3/adapter_model.bin
+
+# Adding an adapter to the original FLAN-T5 model
+from peft import PeftModel, PeftConfig
+
+peft_model_base = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base", torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+
+peft_model = PeftModel.from_pretrained(peft_model_base, 
+                                       './peft-dialogue-summary-checkpoint-from-s3/', 
+                                       torch_dtype=torch.bfloat16,
+                                       is_trainable=False) # If you were preparing the model for further training, you would set is_trainable=True
+
+print(print_number_of_trainable_model_parameters(peft_model)) # The number of trainable parameters will be 0 due to is_trainable=False setting
+
+
+# Evaluate the Model Qualitatively (Human Evaluation)
+index = 200
+dialogue = dataset['test'][index]['dialogue']
+baseline_human_summary = dataset['test'][index]['summary']
+
+prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary: """
+
+input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+
+instruct_model_outputs = instruct_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
+
+peft_model_outputs = peft_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200, num_beams=1))
+peft_model_text_output = tokenizer.decode(peft_model_outputs[0], skip_special_tokens=True)
+
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{human_baseline_summary}')
+print(dash_line)
+print(f'ORIGINAL MODEL:\n{original_model_text_output}')
+print(dash_line)
+print(f'INSTRUCT MODEL:\n{instruct_model_text_output}')
+print(dash_line)
+print(f'PEFT MODEL: {peft_model_text_output}')
+
+
+# Evaluate the Model Quantitatively (with ROUGE Metric)
+dialogues = dataset['test'][0:10]['dialogue']
+human_baseline_summaries = dataset['test'][0:10]['summary']
+
+original_model_summaries = []
+instruct_model_summaries = []
+peft_model_summaries = []
+
+for idx, dialogue in enumerate(dialogues):
+    prompt = f"""
+Summarize the following conversation.
+
+{dialogue}
+
+Summary: """
+    
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    human_baseline_text_output = human_baseline_summaries[idx]
+    
+    original_model_outputs = original_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    original_model_text_output = tokenizer.decode(original_model_outputs[0], skip_special_tokens=True)
+
+    instruct_model_outputs = instruct_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    instruct_model_text_output = tokenizer.decode(instruct_model_outputs[0], skip_special_tokens=True)
+
+    peft_model_outputs = peft_model.generate(input_ids=input_ids, generation_config=GenerationConfig(max_new_tokens=200))
+    peft_model_text_output = tokenizer.decode(peft_model_outputs[0], skip_special_tokens=True)
+
+    original_model_summaries.append(original_model_text_output)
+    instruct_model_summaries.append(instruct_model_text_output)
+    peft_model_summaries.append(peft_model_text_output)
+
+zipped_summaries = list(zip(human_baseline_summaries, original_model_summaries, instruct_model_summaries, peft_model_summaries))
+ 
+df = pd.DataFrame(zipped_summaries, columns = ['human_baseline_summaries', 'original_model_summaries', 'instruct_model_summaries', 'peft_model_summaries'])
+df
+
+rouge = evaluate.load('rouge')
+
+original_model_results = rouge.compute(
+    predictions=original_model_summaries,
+    references=human_baseline_summaries[0:len(original_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+instruct_model_results = rouge.compute(
+    predictions=instruct_model_summaries,
+    references=human_baseline_summaries[0:len(instruct_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+peft_model_results = rouge.compute(
+    predictions=peft_model_summaries,
+    references=human_baseline_summaries[0:len(peft_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('ORIGINAL MODEL:')
+print(original_model_results)
+print('INSTRUCT MODEL:')
+print(instruct_model_results)
+print('PEFT MODEL:')
+print(peft_model_results)
+
+
+# Compare to other models
+human_baseline_summaries = results['human_baseline_summaries'].values
+original_model_summaries = results['original_model_summaries'].values
+instruct_model_summaries = results['instruct_model_summaries'].values
+peft_model_summaries     = results['peft_model_summaries'].values
+
+original_model_results = rouge.compute(
+    predictions=original_model_summaries,
+    references=human_baseline_summaries[0:len(original_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+instruct_model_results = rouge.compute(
+    predictions=instruct_model_summaries,
+    references=human_baseline_summaries[0:len(instruct_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+peft_model_results = rouge.compute(
+    predictions=peft_model_summaries,
+    references=human_baseline_summaries[0:len(peft_model_summaries)],
+    use_aggregator=True,
+    use_stemmer=True,
+)
+
+print('ORIGINAL MODEL:')
+print(original_model_results)
+print('INSTRUCT MODEL:')
+print(instruct_model_results)
+print('PEFT MODEL:')
+print(peft_model_results)
+
+
+# Improvement of PEFT over the original model
+print("Absolute percentage improvement of PEFT MODEL over ORIGINAL MODEL")
+
+improvement = (np.array(list(peft_model_results.values())) - np.array(list(original_model_results.values())))
+for key, value in zip(peft_model_results.keys(), improvement):
+    print(f'{key}: {value*100:.2f}%')
+
+# Improvement of PEFT over a full fine-tuned model
+print("Absolute percentage improvement of PEFT MODEL over INSTRUCT MODEL")
+
+improvement = (np.array(list(peft_model_results.values())) - np.array(list(instruct_model_results.values())))
+for key, value in zip(peft_model_results.keys(), improvement):
+    print(f'{key}: {value*100:.2f}%')
