2c278a8d3a5ce132fc9d36694aa20817249bde4b
Paula <32401901+paula1999@users.noreply.github.com>
2024-04-17 13:43:57 +0200

    Create DCGAN.py

---
+++ GenAI/DCGAN.py
@@ -0,0 +1,297 @@
+# Deep Convolutional GAN
+
+'''
+Architecture guidelines for stable Deep Convolutional GANs
+*   Use convolutions without any pooling layers
+*   Use batchnorm in both the generator and the discriminator
+*   Don't use fully connected hidden layers
+*   Use ReLU activation in the generator for all layers except for the output, which uses a Tanh activation.
+*   Use LeakyReLU activation in the discriminator for all layers except for the output, which does not use an activation
+'''
+
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0) # Set for testing purposes
+
+
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+
+
+######################################
+# Generator
+######################################
+
+'''
+Instead of passing the image dimension, you will pass the number of image channels to the generator.
+With DCGAN you use convolutions which don't depend on the number of pixel of an image.
+The number of channels is important to determine the size of the filters.
+
+Build a generator using 4 layers (3 hidden layers + 1 output layer).
+Write a function to create a single block for the generator's neural network.
+
+Since in DCGAN the activation function will be different for the output layer, 
+you will need to check what layer is being created.
+'''
+
+class Generator(nn.Module):
+    '''
+    Generator Class
+    Values:
+        z_dim: the dimension of the noise vector, a scalar
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (MNIST is black-and-white, so 1 channel is your default)
+        hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, z_dim=10, im_chan=1, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+        # Build the neural network
+        self.gen = nn.Sequential(
+            self.make_gen_block(z_dim, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2, kernel_size=4, stride=1),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a generator block of DCGAN, 
+        corresponding to a transposed convolution, a batchnorm (except for in the last layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+
+        #     Steps:
+        #       1) Do a transposed convolution using the given parameters.
+        #       2) Do a batchnorm, except for the last layer.
+        #       3) Follow each batchnorm with a ReLU activation.
+        #       4) If its the final layer, use a Tanh activation after the deconvolution.
+
+        # Build the neural block
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride=stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True)
+            )
+        else: # Final Layer
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride=stride),
+                nn.Tanh()
+            )
+
+    def unsqueeze_noise(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor, 
+        returns a copy of that noise with width and height = 1 and channels = z_dim.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        return noise.view(len(noise), self.z_dim, 1, 1)
+
+    def forward(self, noise):
+        '''
+        Function for completing a forward pass of the generator: Given a noise tensor, 
+        returns generated images.
+        Parameters:
+            noise: a noise tensor with dimensions (n_samples, z_dim)
+        '''
+        x = self.unsqueeze_noise(noise)
+        return self.gen(x)
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    '''
+    Function for creating noise vectors: Given the dimensions (n_samples, z_dim)
+    creates a tensor of that shape filled with random numbers from the normal distribution.
+    Parameters:
+        n_samples: the number of samples to generate, a scalar
+        z_dim: the dimension of the noise vector, a scalar
+        device: the device type
+    '''
+    return torch.randn(n_samples, z_dim, device=device)
+
+
+
+######################################
+# Discriminator
+######################################
+
+'''
+Use 3 layers and create a function to create a single neural network block
+'''
+
+class Discriminator(nn.Module):
+    '''
+    Discriminator Class
+    Values:
+        im_chan: the number of channels in the images, fitted for the dataset used, a scalar
+              (MNIST is black-and-white, so 1 channel is your default)
+    hidden_dim: the inner dimension, a scalar
+    '''
+    def __init__(self, im_chan=1, hidden_dim=16):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            self.make_disc_block(im_chan, hidden_dim),
+            self.make_disc_block(hidden_dim, hidden_dim * 2),
+            self.make_disc_block(hidden_dim * 2, 1, final_layer=True),
+        )
+
+    def make_disc_block(self, input_channels, output_channels, kernel_size=4, stride=2, final_layer=False):
+        '''
+        Function to return a sequence of operations corresponding to a discriminator block of DCGAN, 
+        corresponding to a convolution, a batchnorm (except for in the last layer), and an activation.
+        Parameters:
+            input_channels: how many channels the input feature representation has
+            output_channels: how many channels the output feature representation should have
+            kernel_size: the size of each convolutional filter, equivalent to (kernel_size, kernel_size)
+            stride: the stride of the convolution
+            final_layer: a boolean, true if it is the final layer and false otherwise 
+                      (affects activation and batchnorm)
+        '''
+        #     Steps:
+        #       1) Add a convolutional layer using the given parameters.
+        #       2) Do a batchnorm, except for the last layer.
+        #       3) Follow each batchnorm with a LeakyReLU activation with slope 0.2.
+        #       Note: Don't use an activation on the final layer
+        
+        # Build the neural block
+        if not final_layer:
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride=stride),
+                nn.BatchNorm2d(output_channels),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            )
+        else: # Final Layer
+            return nn.Sequential(
+                nn.Conv2d(input_channels, output_channels, kernel_size, stride=stride)
+            )
+
+    def forward(self, image):
+        '''
+        Function for completing a forward pass of the discriminator: Given an image tensor, 
+        returns a 1-dimension tensor representing fake/real.
+        Parameters:
+            image: a flattened image tensor with dimension (im_dim)
+        '''
+        disc_pred = self.disc(image)
+        return disc_pred.view(len(disc_pred), -1)
+
+
+
+######################################
+# Training
+######################################
+
+criterion = nn.BCEWithLogitsLoss() # loss function
+z_dim = 64 # dimension of the noise vector
+display_step = 500 # how often to display/visualize the images
+batch_size = 128 # number of images per forward/backward pass
+# A learning rate of 0.0002 works well on DCGAN
+lr = 0.0002 # learning rate
+
+# These parameters control the optimizer's momentum
+beta_1 = 0.5 # momentum term
+beta_2 = 0.999 # momentum term
+device = 'cuda' # device type
+
+# You can tranform the image values to be between -1 and 1 (the range of the tanh activation)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),
+])
+
+dataloader = DataLoader(
+    MNIST('.', download=False, transform=transform),
+    batch_size=batch_size,
+    shuffle=True)
+
+
+# Initialize generator, discriminator and optimizers
+gen = Generator(z_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+disc = Discriminator().to(device) 
+disc_opt = torch.optim.Adam(disc.parameters(), lr=lr, betas=(beta_1, beta_2))
+
+# You initialize the weights to the normal distribution
+# with mean 0 and standard deviation 0.02
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+gen = gen.apply(weights_init)
+disc = disc.apply(weights_init)
+
+
+
+# For each epcoh, process theentire dataset in batches
+# For every batch, update the discriminator and generator
+n_epochs = 50 # number of times you iterate through the entire dataset when training
+cur_step = 0
+mean_generator_loss = 0
+mean_discriminator_loss = 0
+for epoch in range(n_epochs):
+    # Dataloader returns the batches
+    for real, _ in tqdm(dataloader):
+        cur_batch_size = len(real)
+        real = real.to(device)
+
+        ## Update discriminator ##
+        disc_opt.zero_grad()
+        fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+        fake = gen(fake_noise)
+        disc_fake_pred = disc(fake.detach())
+        disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+        disc_real_pred = disc(real)
+        disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+        disc_loss = (disc_fake_loss + disc_real_loss) / 2
+
+        # Keep track of the average discriminator loss
+        mean_discriminator_loss += disc_loss.item() / display_step
+        # Update gradients
+        disc_loss.backward(retain_graph=True)
+        # Update optimizer
+        disc_opt.step()
+
+        ## Update generator ##
+        gen_opt.zero_grad()
+        fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
+        fake_2 = gen(fake_noise_2)
+        disc_fake_pred = disc(fake_2)
+        gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+        gen_loss.backward()
+        gen_opt.step()
+
+        # Keep track of the average generator loss
+        mean_generator_loss += gen_loss.item() / display_step
+
+        ## Visualization code ##
+        if cur_step % display_step == 0 and cur_step > 0:
+            print(f"Epoch {epoch}, step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            mean_generator_loss = 0
+            mean_discriminator_loss = 0
+        cur_step += 1
