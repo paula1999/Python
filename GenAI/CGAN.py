d7dc8e1dffb41e39cc7c2de976a90ee8f1995bdf
Paula <32401901+paula1999@users.noreply.github.com>
2024-04-22 11:37:32 +0200

    Update CGAN.py

--- GenAI/CGAN.py
+++ GenAI/CGAN.py
@@ -220,4 +220,265 @@ dataloader = DataLoader(
     shuffle=True)
 
 
+'''
+Initialize generator, discriminator and optimizers.
+Update the input dimensions for both models.
+For the generator, you will need to calculate the size of the input vector; 
+recall that for conditional GANs, the generator's input is the noise vector concatenated with the class vector. 
+For the discriminator, you need to add a channel for every class.
+'''
 
+def get_input_dimensions(z_dim, mnist_shape, n_classes):
+    '''
+    Function for getting the size of the conditional input dimensions 
+    from z_dim, the image shape, and number of classes.
+    Parameters:
+        z_dim: the dimension of the noise vector, a scalar
+        mnist_shape: the shape of each MNIST image as (C, W, H), which is (1, 28, 28)
+        n_classes: the total number of classes in the dataset, an integer scalar
+                (10 for MNIST)
+    Returns: 
+        generator_input_dim: the input dimensionality of the conditional generator, 
+                          which takes the noise and class vectors
+        discriminator_im_chan: the number of input channels to the discriminator
+                            (e.g. C x 28 x 28 for MNIST)
+    '''
+
+    generator_input_dim = z_dim + n_classes
+    discriminator_im_chan = mnist_shape[0] + n_classes
+
+    return generator_input_dim, discriminator_im_chan
+
+
+generator_input_dim, discriminator_im_chan = get_input_dimensions(z_dim, mnist_shape, n_classes)
+
+gen = Generator(input_dim=generator_input_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+disc = Discriminator(im_chan=discriminator_im_chan).to(device)
+disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+gen = gen.apply(weights_init)
+disc = disc.apply(weights_init)
+
+
+'''
+To train, your generator and your discriminator need to know what class of image should be generated. 
+'''
+
+# UNQ_C4 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED CELL
+cur_step = 0
+generator_losses = []
+discriminator_losses = []
+
+#UNIT TEST NOTE: Initializations needed for grading
+noise_and_labels = False
+fake = False
+
+fake_image_and_labels = False
+real_image_and_labels = False
+disc_fake_pred = False
+disc_real_pred = False
+
+for epoch in range(n_epochs):
+    # Dataloader returns the batches and the labels
+    for real, labels in tqdm(dataloader):
+        cur_batch_size = len(real)
+        # Flatten the batch of real images from the dataset
+        real = real.to(device)
+
+        one_hot_labels = get_one_hot_labels(labels.to(device), n_classes)
+        image_one_hot_labels = one_hot_labels[:, :, None, None]
+        image_one_hot_labels = image_one_hot_labels.repeat(1, 1, mnist_shape[1], mnist_shape[2])
+
+        ### Update discriminator ###
+        # Zero out the discriminator gradients
+        disc_opt.zero_grad()
+        # Get noise corresponding to the current batch_size 
+        fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+        
+        # Now you can get the images from the generator
+        # Steps: 1) Combine the noise vectors and the one-hot labels for the generator
+        #        2) Generate the conditioned fake images
+        noise_and_labels = combine_vectors(fake_noise, one_hot_labels)
+        fake = gen(noise_and_labels)
+        
+        # Make sure that enough images were generated
+        assert len(fake) == len(real)
+        # Check that correct tensors were combined
+        assert tuple(noise_and_labels.shape) == (cur_batch_size, fake_noise.shape[1] + one_hot_labels.shape[1])
+        # It comes from the correct generator
+        assert tuple(fake.shape) == (len(real), 1, 28, 28)
+
+        # Now you can get the predictions from the discriminator
+        # Steps: 1) Create the input for the discriminator
+        #           a) Combine the fake images with image_one_hot_labels, 
+        #              remember to detach the generator (.detach()) so you do not backpropagate through it
+        #           b) Combine the real images with image_one_hot_labels
+        #        2) Get the discriminator's prediction on the fakes as disc_fake_pred
+        #        3) Get the discriminator's prediction on the reals as disc_real_pred
+        fake_image_and_labels = combine_vectors(fake.detach(), image_one_hot_labels)
+        real_image_and_labels = combine_vectors(real, image_one_hot_labels)
+        disc_fake_pred = disc(fake_image_and_labels)
+        disc_real_pred = disc(real_image_and_labels)
+        
+        # Make sure shapes are correct 
+        assert tuple(fake_image_and_labels.shape) == (len(real), fake.detach().shape[1] + image_one_hot_labels.shape[1], 28 ,28)
+        assert tuple(real_image_and_labels.shape) == (len(real), real.shape[1] + image_one_hot_labels.shape[1], 28 ,28)
+        # Make sure that enough predictions were made
+        assert len(disc_real_pred) == len(real)
+        # Make sure that the inputs are different
+        assert torch.any(fake_image_and_labels != real_image_and_labels)
+        # Shapes must match
+        assert tuple(fake_image_and_labels.shape) == tuple(real_image_and_labels.shape)
+        assert tuple(disc_fake_pred.shape) == tuple(disc_real_pred.shape)
+        
+        
+        disc_fake_loss = criterion(disc_fake_pred, torch.zeros_like(disc_fake_pred))
+        disc_real_loss = criterion(disc_real_pred, torch.ones_like(disc_real_pred))
+        disc_loss = (disc_fake_loss + disc_real_loss) / 2
+        disc_loss.backward(retain_graph=True)
+        disc_opt.step() 
+
+        # Keep track of the average discriminator loss
+        discriminator_losses += [disc_loss.item()]
+
+        ### Update generator ###
+        # Zero out the generator gradients
+        gen_opt.zero_grad()
+
+        fake_image_and_labels = combine_vectors(fake, image_one_hot_labels)
+        # This will error if you didn't concatenate your labels to your image correctly
+        disc_fake_pred = disc(fake_image_and_labels)
+        gen_loss = criterion(disc_fake_pred, torch.ones_like(disc_fake_pred))
+        gen_loss.backward()
+        gen_opt.step()
+
+        # Keep track of the generator losses
+        generator_losses += [gen_loss.item()]
+
+        if cur_step % display_step == 0 and cur_step > 0:
+            gen_mean = sum(generator_losses[-display_step:]) / display_step
+            disc_mean = sum(discriminator_losses[-display_step:]) / display_step
+            print(f"Epoch {epoch}, step {cur_step}: Generator loss: {gen_mean}, discriminator loss: {disc_mean}")
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            step_bins = 20
+            x_axis = sorted([i * step_bins for i in range(len(generator_losses) // step_bins)] * step_bins)
+            num_examples = (len(generator_losses) // step_bins) * step_bins
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Generator Loss"
+            )
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(discriminator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Discriminator Loss"
+            )
+            plt.legend()
+            plt.show()
+        elif cur_step == 0:
+            print("Congratulations! If you've gotten here, it's working. Please let this train until you're happy with how the generated numbers look, and then go on to the exploration!")
+        cur_step += 1
+
+
+
+
+
+
+# Exploration
+
+# Before you explore, you should put the generator
+# in eval mode, both in general and so that batch norm
+# doesn't cause you issues and is using its eval statistics
+gen = gen.eval()
+
+# Changing the Class Vector
+
+'''
+You can generate some numbers with your new model! You can add interpolation as well to make it more interesting.
+
+So starting from a image, you will produce intermediate images that look more and more like the ending image until you get to the final image. 
+Your're basically morphing one image into another. You can choose what these two images will be using your conditional GAN.
+'''
+import math
+
+### Change me! ###
+n_interpolation = 9 # Choose the interpolation: how many intermediate images you want + 2 (for the start and end image)
+interpolation_noise = get_noise(1, z_dim, device=device).repeat(n_interpolation, 1)
+
+def interpolate_class(first_number, second_number):
+    first_label = get_one_hot_labels(torch.Tensor([first_number]).long(), n_classes)
+    second_label = get_one_hot_labels(torch.Tensor([second_number]).long(), n_classes)
+
+    # Calculate the interpolation vector between the two labels
+    percent_second_label = torch.linspace(0, 1, n_interpolation)[:, None]
+    interpolation_labels = first_label * (1 - percent_second_label) + second_label * percent_second_label
+
+    # Combine the noise and the labels
+    noise_and_labels = combine_vectors(interpolation_noise, interpolation_labels.to(device))
+    fake = gen(noise_and_labels)
+    show_tensor_images(fake, num_images=n_interpolation, nrow=int(math.sqrt(n_interpolation)), show=False)
+
+### Change me! ###
+start_plot_number = 1 # Choose the start digit
+### Change me! ###
+end_plot_number = 5 # Choose the end digit
+
+plt.figure(figsize=(8, 8))
+interpolate_class(start_plot_number, end_plot_number)
+_ = plt.axis('off')
+
+### Uncomment the following lines of code if you would like to visualize a set of pairwise class 
+### interpolations for a collection of different numbers, all in a single grid of interpolations.
+### You'll also see another visualization like this in the next code block!
+# plot_numbers = [2, 3, 4, 5, 7]
+# n_numbers = len(plot_numbers)
+# plt.figure(figsize=(8, 8))
+# for i, first_plot_number in enumerate(plot_numbers):
+#     for j, second_plot_number in enumerate(plot_numbers):
+#         plt.subplot(n_numbers, n_numbers, i * n_numbers + j + 1)
+#         interpolate_class(first_plot_number, second_plot_number)
+#         plt.axis('off')
+# plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0.1, wspace=0)
+# plt.show()
+# plt.close()
+
+
+
+# Changing the Noise Vector
+n_interpolation = 9 # How many intermediate images you want + 2 (for the start and end image)
+
+# This time you're interpolating between the noise instead of the labels
+interpolation_label = get_one_hot_labels(torch.Tensor([5]).long(), n_classes).repeat(n_interpolation, 1).float()
+
+def interpolate_noise(first_noise, second_noise):
+    # This time you're interpolating between the noise instead of the labels
+    percent_first_noise = torch.linspace(0, 1, n_interpolation)[:, None].to(device)
+    interpolation_noise = first_noise * percent_first_noise + second_noise * (1 - percent_first_noise)
+
+    # Combine the noise and the labels again
+    noise_and_labels = combine_vectors(interpolation_noise, interpolation_label.to(device))
+    fake = gen(noise_and_labels)
+    show_tensor_images(fake, num_images=n_interpolation, nrow=int(math.sqrt(n_interpolation)), show=False)
+
+# Generate noise vectors to interpolate between
+### Change me! ###
+n_noise = 5 # Choose the number of noise examples in the grid
+plot_noises = [get_noise(1, z_dim, device=device) for i in range(n_noise)]
+plt.figure(figsize=(8, 8))
+for i, first_plot_noise in enumerate(plot_noises):
+    for j, second_plot_noise in enumerate(plot_noises):
+        plt.subplot(n_noise, n_noise, i * n_noise + j + 1)
+        interpolate_noise(first_plot_noise, second_plot_noise)
+        plt.axis('off')
+plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0.1, wspace=0)
+plt.show()
+plt.close()
